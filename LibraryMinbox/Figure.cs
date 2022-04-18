8e87a17e566bf630672d45d5fa9174ebcd887615
Constantine <rakin.rolll@inbox.ru>
2022-04-18 15:26:50 +0200

    Добавьте файлы проекта.

---
+++ LibraryMinbox/Figure.cs
@@ -0,0 +1,135 @@
+﻿namespace LibraryMinbox
+{
+    /// <summary>
+    /// Перечисление типа фигуры
+    /// </summary>
+    public enum TypeFigure
+    {
+        Circle = 1,
+        Triangle = 3
+    }
+
+    /// <summary>
+    /// Класс для взаимодействия с фигурами
+    /// </summary>
+    public static class Figure
+    {
+        /// <summary>
+        /// Получение площади фигуры
+        /// </summary>
+        /// <param name="items">Список длин сторон фигуры</param>
+        /// <returns>Площадь фигуры</returns>
+        /// <exception cref="ArgumentException">Если данных не хватает для работы метода</exception>
+        public static double GetArea(List<int> items)
+        {
+            int count = items.Count;
+
+            switch (count)
+            {
+                case (int)TypeFigure.Circle:
+                    return AreaCircle(items[0]);
+
+                case (int)TypeFigure.Triangle:
+                    return AreaTriangle(items);
+
+                default: throw new ArgumentException("Недостаточно данных для определения площади");
+            }
+        }
+
+        /// <summary>
+        /// Получение площади фигуры, по заранее известному типу фигуры
+        /// </summary>
+        /// <param name="typeFigure">Тип фигуры</param>
+        /// <param name="items">Список длин сторон</param>
+        /// <returns>Площадь фигуры</returns>
+        /// <exception cref="ArgumentException">Если данных не хватает для работы метода</exception>
+        public static double GetArea(TypeFigure typeFigure, List<int> items)
+        {
+            switch (typeFigure)
+            {
+                case TypeFigure.Circle:
+                    return AreaCircle(items[0]);
+
+                case TypeFigure.Triangle:
+                    return AreaTriangle(items);
+
+                default: throw new ArgumentException("Недостаточно данных для определения площади");
+            }
+        }
+
+        /// <summary>
+        /// Метод проверяет является ли треугольник прямоугльным
+        /// </summary>
+        /// <param name="items">Список длин сторон треугольника</param>
+        /// <returns>Принадлежность к прямоугольному треугольнику</returns>
+        /// <exception cref="ArgumentException"></exception>
+        public static bool IsRightTriangle(List<int> items)
+        {
+            if (items.Count == 3)
+            {
+                int a = items[0];
+                int b = items[1];
+                int c = items[2];
+                if (a * a + b * b == c * c) return true;
+                else if (a * a + c * c == b * b) return true;
+                else if (c * c + b * b == a * a) return true;
+                else return false;
+            }
+            else throw new ArgumentException("Эта фигура не является треугольником");
+        }
+
+        /// <summary>
+        /// Метод проверяет является ли треугольник прямоугольным
+        /// </summary>
+        /// <param name="typeFigure">Тип фигуры</param>
+        /// <param name="items">Список длин сторон треугольника</param>
+        /// <returns>Принадлежность к прямоугольному треугольнику</returns>
+        /// <exception cref="ArgumentException"></exception>
+        public static bool IsRightTriangle(TypeFigure typeFigure, List<int> items)
+        {
+            if (typeFigure == TypeFigure.Triangle)
+            {
+                int a = items[0];
+                int b = items[1];
+                int c = items[2];
+                if (a * a + b * b == c * c) return true;
+                else if (a * a + c * c == b * b) return true;
+                else if (c * c + b * b == a * a) return true;
+                else return false;
+            }
+            else throw new ArgumentException("Эта фигура не является треугольником");
+        }
+
+        /// <summary>
+        /// Считает площадь по радиусу окружгности
+        /// </summary>
+        /// <param name="r">Радиус окружности</param>
+        /// <returns>Площадь, округленную до 6 знаов после запятой</returns>
+        private static double AreaCircle(int r) 
+        {
+            return Math.Round(Math.PI* Math.Pow(r, 2), 6);
+        }
+
+        /// <summary>
+        /// Считает пощадь через три стороны треугольника
+        /// </summary>
+        /// <param name="items">Список сторон треугольника</param>
+        /// <returns>Площадь, округленную до 6 знаков после запятой</returns>
+        private static double AreaTriangle(List<int> items)
+        {
+            int perimeter = 0;
+            foreach (int item in items)
+            {
+                perimeter += item;
+            }
+            double p = perimeter / 2;
+            var a = p - items[0];
+            var b = p - items[1];
+            var c  = p - items[2];
+
+            var result = Math.Sqrt(p * a * b * c);
+            return Math.Round(result, 6);
+        }
+
+    }
+}
---
+++ LibraryMinboxTests/FigureTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LibraryMinbox;
+using System;
+using System.Collections.Generic;
+
+namespace LibraryMinbox.Tests
+{
+    [TestClass()]
+    public class FigureTests
+    {        
+        [TestMethod()]
+        public void GetAreaTest()
+        {
+            // Arrange
+            List<int> itemsTriangle = new List<int> { 4, 4, 4 };
+            List<int> itemsCircle = new List<int> { 12 };
+            List<int> itemUnknown = new List<int>() { 1, 2, 3, 4, 5, 6, 7 };
+            List<int> rightTraingle = new List<int>() { 3, 4, 5 };
+
+            // Act
+            // Получение площадей через через известную фигуру и неизветсную
+            var resTriangle = Figure.GetArea(TypeFigure.Triangle, itemsTriangle);
+            var resTriangleUnk = Figure.GetArea(itemsTriangle);
+            
+            var resCircle = Figure.GetArea(TypeFigure.Circle, itemsCircle);
+            var resCircleUnk = Figure.GetArea(itemsCircle);
+
+            // Является ли треугольник прямоугольным
+            var isRightTriangle = Figure.IsRightTriangle(rightTraingle);
+            //var resUnk = Figure.GetArea(itemUnknown);
+            
+            // Assert
+            Assert.AreEqual(6.928203, resTriangle);
+            Assert.AreEqual(6.928203, resTriangleUnk);
+            Assert.AreEqual(452.389342, resCircle);
+            Assert.AreEqual(452.389342, resCircleUnk);
+            Assert.IsTrue(isRightTriangle);
+            //Console.WriteLine(resUnk);
+        }
+    }
+}
